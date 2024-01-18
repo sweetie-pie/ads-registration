890e8a726eff699e21ed531240680a07b3bcd887
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-18 11:45:47 +0330

    add: middleware for jwt

--- internal/http/middleware.go
+++ internal/http/middleware.go
@@ -2,6 +2,17 @@ package http
 
 import "github.com/gofiber/fiber/v2"
 
+func JWTToken(ctx *fiber.Ctx) error {
+	token := ctx.Get("x-token", "null")
+	if token == "null" {
+		return fiber.ErrUnauthorized
+	}
+
+	// todo: get user claims from x-token
+
+	return ctx.Next()
+}
+
 func CheckAdmin(ctx *fiber.Ctx) error {
 	claims := ctx.Locals("user").(*UserClaims)
 
