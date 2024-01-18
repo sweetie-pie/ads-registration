cec4ee26566bf1973fd735fb8e61b0cde7d22469
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-18 11:40:20 +0330

    add: middlewares

--- internal/http/middleware.go
+++ internal/http/middleware.go
@@ -1,1 +1,37 @@
 package http
+
+import "github.com/gofiber/fiber/v2"
+
+func CheckAdmin(ctx *fiber.Ctx) error {
+	claims := ctx.Locals("user").(*UserClaims)
+
+	if claims.IsAdmin && claims.Active {
+		return ctx.Next()
+	}
+
+	return fiber.ErrForbidden
+}
+
+func CheckBannedUser(ctx *fiber.Ctx) error {
+	claims := ctx.Locals("user").(*UserClaims)
+
+	if !claims.Banned {
+		return ctx.Next()
+	}
+
+	return fiber.ErrForbidden
+}
+
+func CheckAccessLevel(levels ...int) func(ctx *fiber.Ctx) error {
+	return func(ctx *fiber.Ctx) error {
+		claims := ctx.Locals("user").(*UserClaims)
+
+		for _, level := range levels {
+			if level == claims.AccessLevel {
+				return ctx.Next()
+			}
+		}
+
+		return fiber.ErrForbidden
+	}
+}
--- internal/http/request.go
+++ internal/http/request.go
@@ -12,6 +12,14 @@ type UserMetaRequest struct {
 	AccessLevel int  `json:"access_level"`
 }
 
+type UserClaims struct {
+	Username    string `json:"username"`
+	IsAdmin     bool   `json:"is_admin"`
+	Banned      bool   `json:"banned"`
+	Active      bool   `json:"active"`
+	AccessLevel int    `json:"access_level"`
+}
+
 type CategoryRequest struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
