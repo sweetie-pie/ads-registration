dc082e4f203ffae0dba769ec58ecfef7faede311
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-18 11:32:15 +0330

    add: response

--- internal/http/response.go
+++ internal/http/response.go
@@ -1,1 +1,38 @@
 package http
+
+import "time"
+
+type UserResponse struct {
+	ID        uint      `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Banned    bool      `json:"banned"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type AdminResponse struct {
+	ID          uint      `json:"id"`
+	Username    string    `json:"username"`
+	Email       string    `json:"email"`
+	Active      bool      `json:"active"`
+	AccessLevel int       `json:"access_level"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+type AdResponse struct {
+	ID          uint      `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Status      int       `json:"status"`
+	Image       string    `json:"image"`
+	Username    string    `json:"username"`
+	Admin       string    `json:"admin"`
+	Categories  []string  `json:"categories"`
+	CreatedAt   time.Time `json:"created_at"`
+}
+
+type CategoryResponse struct {
+	ID          uint   `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
