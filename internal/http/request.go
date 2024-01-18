b573e61568dc1392b997ccaa5401a19a14edd5b8
amirhnajafiz <najafizadeh21@gmail.com>
2024-01-18 11:27:39 +0330

    add: requests

--- internal/http/request.go
+++ internal/http/request.go
@@ -1,1 +1,18 @@
 package http
+
+type UserRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
+
+type UserMetaRequest struct {
+	Banned      bool `json:"banned"`
+	Active      bool `json:"active"`
+	AccessLevel int  `json:"access_level"`
+}
+
+type CategoryRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
